b80eec20e56b04c6a70f0396cb0113ef0378cbd7
daenamcclintock <daena.mcclintock@gmail.com>
2022-10-11 21:23:03 -0500

    edits

--- components/Landing/Coin/Three.js
+++ components/Landing/Coin/Three.js
@@ -2,14 +2,11 @@ import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import Bitcoin from './Bitcoin.png'
 import * as THREE from 'three'
-import { NavItem } from 'react-bootstrap'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { MdDoNotDisturbOnTotalSilence } from 'react-icons/md'
 
 const Three = (props) => {
-    const bitcoin = require('./Bitcoin.png');
-    console.log('this is bitcoin', bitcoin)
-    bitcoin.width = 1
-    bitcoin.height = 1
+    console.log('this is bitcoin', Bitcoin)
     // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
@@ -18,18 +15,17 @@ const Three = (props) => {
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (ref.current.rotation.x += 0.02, ref.current.rotation.y += 0.05))
   // Return the view, these are regular Threejs elements expressed in JSX
-  const bitcoinTexture = new THREE.TextureLoader().load(bitcoin);
+  const bitcoinTexture = new THREE.TextureLoader().load(Bitcoin);
   const material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
       map: bitcoinTexture
   })
   console.log(material)
   Bitcoin.width = 1
   Bitcoin.height = 1
-  const etherTexter = new THREE.TextureLoader().load(Bitcoin)
+  const etherTexter = new THREE.TextureLoader().load('https://bitcoin.org/img/icons/opengraph.png?1664356125')
   console.log(bitcoinTexture)
   console.log(etherTexter)
-  console.log(bitcoin)
-  console.log(Bitcoin)
 
   const loader = new THREE.TextureLoader()
   loader.load(Bitcoin, function ( texture ) {
@@ -38,6 +34,8 @@ const Three = (props) => {
       })
       console.log(material)
   })
+
+  // const colorMap = useLoader(bitcoinTexture)
   
 
   return (
@@ -48,8 +46,8 @@ const Three = (props) => {
       onClick={(e) => click(!clicked)}
       onPointerOver={(e) => hover(true)}
       onPointerOut={(e) => hover(false)}>
-      <cylinderGeometry args={[0.7, 0.7, 0.12, 100]} />
-      <meshStandardMaterial metalness={0.7} roughness={0.3} />
+      <cylinderGeometry args={[0.6, 0.6, 0.06, 100]} />
+      <meshStandardMaterial metalness={0.7} roughness={0.3}/>
     </mesh>
   )
 }
--- components/Landing/Hero/Hero.js
+++ components/Landing/Hero/Hero.js
@@ -3,7 +3,8 @@ import { HeroContainer, HeroTitleContainer, HeroTitle } from './HeroStyles'
 import { Hero } from '@web3uikit/core'
 import Coin from '../Coin/Coin'
 import Box from '../Coin/Box'
-import { Canvas, useFrame } from '@react-three/fiber'
+import Three from '../Coin/Three'
+import { Canvas, useFrame, Suspense } from '@react-three/fiber'
 
 const Heros = () => {
   return (
@@ -11,15 +12,23 @@ const Heros = () => {
       <Hero
         // backgroundURL='http://clevadesk.com/wp-content/uploads/2018/11/home-page-hero-background-1030x378.jpeg'
         // backgroundURL='/images/shopify.png'
-        backgroundColor='rgb(120, 120, 120)'
-        title='Google x Moralis Hackathon'
+        backgroundColor='rgb(138, 2, 217)'
+        title='Tokenz'
         subTitle='Shopify Token Rewards'
         textColor='white'
         height='66vh'
-        padding='10px'
+        padding='20px'
         align='right'
         // customImage={{url: "/images/DeFi.png"}}
-      />
+      >
+        <Canvas>
+        <ambientLight color={0xffffff} intensity={0.5} />
+        <pointLight color={0xff4422} intensity={1} position={[-1, -1, 3]} />
+        <pointLight color={0x44ff88} intensity={1} position={[1, 2, 3]} />
+        <pointLight color={0xdd3311} intensity={1} position={[0, 3, 2]} />
+        <Three position={[0, 0.3, 3]} />
+        </Canvas>
+      </Hero>
       {/* <Canvas>
         <ambientLight color={0xffffff} intensity={0.5} />
         <pointLight color={0xff4422} intensity={1} position={[-1, -1, 3]} />
@@ -29,12 +38,6 @@ const Heros = () => {
         <Coin position={[-3, 1, 2]}/>
         </Suspense>
       </Canvas> */}
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
     </>
   )
 }

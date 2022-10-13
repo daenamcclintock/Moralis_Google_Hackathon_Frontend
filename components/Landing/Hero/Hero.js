03a9ef06315a56a2153fb918cefba75295776b07
daenamcclintock <daena.mcclintock@gmail.com>
2022-10-13 18:34:05 -0500

    hero background iamge

--- components/Landing/Hero/Hero.js
+++ components/Landing/Hero/Hero.js
@@ -11,12 +11,12 @@ const Heros = () => {
     <>
       <Hero
         // backgroundURL='http://clevadesk.com/wp-content/uploads/2018/11/home-page-hero-background-1030x378.jpeg'
-        // backgroundURL='/images/shopify.png'
-        backgroundColor='rgb(138, 2, 217)'
+        backgroundURL='/images/hero.jpg'
+        // backgroundColor='rgb(138, 2, 217)'
         title='Tokenz'
         subTitle='Shopify Token Rewards'
         textColor='white'
-        height='66vh'
+        height='69vh'
         padding='20px'
         align='right'
         // customImage={{url: "/images/DeFi.png"}}

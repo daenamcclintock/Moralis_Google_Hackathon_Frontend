a5ea3db9e91be33c8913dece92194265ce99fe9a
daenamcclintock <daena.mcclintock@gmail.com>
2022-10-19 19:06:10 -0500

    added partners component

--- components/Landing/Partners/Partners.js
+++ components/Landing/Partners/Partners.js
@@ -1,14 +1,78 @@
 import React from 'react'
-import { PartnersContainer, PartnersTitleContainer , PartnersTitle} from './PartnersStyles'
+import { PartnersContainer, PartnersTitleContainer , PartnersTitle, DocumentDiv} from './PartnersStyles'
 import { Information, Card, Logo } from 'web3uikit'
+import { Table, Tag, Avatar, Button } from 'web3uikit'
+import { GrDocumentText } from "react-icons/gr"
+import { BiLink, BiLike, BiUser, BiUserCheck } from "react-icons/bi"
+import { FaRegHandshake } from "react-icons/fa"
 
 const Partners = () => {
+
+  const documents = [
+    {name: 'Moralis', type: 'Web3 APIs & SDKs', downloadLink: 'https://www.moralis.io/'},
+    {name: 'Polygon Labs', type: 'Layer I Blockchain', downloadLink: 'https://www.polygonlabs.us/'},
+    {name: 'Andreessen Horowitz', type: 'Venture Capital Fund', downloadLink: 'https://a16z.com/'}
+  ]
+
+  if (!documents) {
+      return (
+          <p>Loading...</p>
+      )
+  }
+
+  if (documents.length == 0) {
+      return (
+          <p>No documents</p>
+      )
+  }
+
+  let data = []
+  if (documents.length > 0) {
+      for (let i = 0; i < documents.length; i++) {
+
+
+          data.push([
+              <FaRegHandshake size={26} theme="image"/>,
+              documents[i].name,
+              <Tag color="blue" text={documents[i].type}/>,
+              <a href={documents[i].downloadLink} style={{ textDecoration: 'none' }} target="_blank" >
+                  <Button
+                      text={<>View Website &nbsp;<BiLink /></>}
+                      // onClick={() => window.location.href = `${documents[i].downloadLink}`}
+                  />
+              </a>
+          ])
+      }
+  }
+
   return (
     <>
     <PartnersTitleContainer>
       <PartnersTitle>Parters</PartnersTitle>
     </PartnersTitleContainer>
-    <PartnersContainer className='gap-4 content-evenly'>
+    <PartnersContainer>
+      <Table
+          columnsConfig="80px 3fr 2fr 2fr 80px"
+          data={data}
+          header={[
+              '',
+              <span>Partnership</span>,
+              <span>Type</span>,
+              <span>Website</span>,
+              ''
+          ]}
+          isColumnSortable={[
+              false,
+              true,
+              true,
+              false
+          ]}
+          maxPages={3}
+          noPagination
+          onPageNumberChanged={function noRefCheck(){}}
+          onRowClick={function noRefCheck(){}}
+          pageSize={5}
+      />
     </PartnersContainer>
     </>
   )
--- components/Landing/Partners/PartnersStyles.js
+++ components/Landing/Partners/PartnersStyles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const PartnersContainer = styled.div`
     display: flex;
+    margin: 2rem;
 `
 
 export const PartnersTitleContainer = styled.div`
@@ -14,4 +15,29 @@ export const PartnersTitle = styled.h1`
     font-weight:bold;
     font-size: 26px;
     font-family: Space Grotesk, sans-serif;
+`
+
+export const DocumentInfoContainer = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+`
+
+export const DocumentDiv = styled.section`
+
+`
+
+export const DocumentNameDiv = styled.div`
+
+`
+
+export const DocumentName = styled.p`
+
+`
+
+export const DownloadButtonDiv = styled.div`
+
+`
+
+export const DownloadButton = styled.button`
+
 `

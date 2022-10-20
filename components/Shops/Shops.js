9d61030253fc7bfe236b71ffd85cc205943cf33b
daenamcclintock <daena.mcclintock@gmail.com>
2022-10-19 19:31:54 -0500

    edits to shops

--- components/Shops/Shops.js
+++ components/Shops/Shops.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ShopsContainer } from './ShopsStyles'
-import { Card, Illustration } from "@web3uikit/core"
+import { Button, Card, Illustration } from "@web3uikit/core"
 
 const Shops = (props) => {
 
@@ -22,14 +22,30 @@ const Shops = (props) => {
                     return (
                     <div className="m-5" style={{width: '250px'}}>
                         <a href={store.link} rel="noopener noreferrer" target="_blank">
-                            <Card 
-                                title={store.storeName}
-                                description={store.description}
+                            <Card
                                 onClick={''}
                             >
                                 <div>
-                                <Illustration height={'180px'} logo={'token'} width={'100%'} />
-                                {/* <img src={store.image}/> */}
+                                    {store.storeName}
+                                </div>
+                                <div>
+                                    {store.description}
+                                </div>
+                                <div>
+                                    <Illustration height={'180px'} logo={'token'} width={'100%'} />
+                                    {/* <img src={store.image}/> */}
+                                </div>
+                                <div style={{ display: 'flex', justifyContent: 'center'}}>
+                                    <a href={store.link} rel="noopener noreferrer" style={{ textDecoration: 'none' }} target="_blank" >
+                                        <Button
+                                            text='View Store'
+                                            customize={{
+                                                backgroundColor: '#9EC7EA',
+                                                onHover: 'darken',
+                                            }}
+                                            theme="custom"
+                                        />
+                                    </a>
                                 </div>
                             </Card>
                         </a>

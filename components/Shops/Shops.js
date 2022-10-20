fe4efca42b7219543b092e7f63a41c2d16b84e4d
daenamcclintock <daena.mcclintock@gmail.com>
2022-10-19 19:49:22 -0500

    edits to shops and shops styles

--- components/Shops/Shops.js
+++ components/Shops/Shops.js
@@ -1,59 +1,58 @@
 import React from 'react'
-import { ShopsContainer } from './ShopsStyles'
-import { Button, Card, Illustration } from "@web3uikit/core"
+import { ButtonDiv, CardDiv, ImageDiv, Image, Link, ShopsContainer, StoreDescriptionDiv, StoreNameDiv, StoresDiv } from './ShopsStyles'
+import { Button, Card } from "@web3uikit/core"
 
 const Shops = (props) => {
 
   const shopifyStores = [
-    {storeName: 'My Store', description: 'This store sells electronics', category: 'electronics', image: '', link: 'https://www.shopify.com/'},
-    {storeName: 'My Store', description: 'This store sells clothes', category: 'clothes', image: '', link: 'https://www.shopify.com/'},
-    {storeName: 'My Store', description: 'This store sells shoes', category: 'shoes', image: '', link: 'https://www.shopify.com/'},
-    {storeName: 'My Store', description: 'This store sells collectibles', category: 'collectibles', image: '', link: 'https://www.shopify.com/'},
-    {storeName: 'My Store', description: 'This store sells handmade crafts', category: 'crafts', image: '', link: 'https://www.shopify.com/'},
-    {storeName: 'My Store', description: 'This store sells used iphones', category: 'electronics', image: '', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells electronics', category: 'electronics', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells clothes', category: 'clothes', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells shoes', category: 'shoes', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells collectibles', category: 'collectibles', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells handmade crafts', category: 'crafts', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
+    {storeName: 'My Store', description: 'This store sells used iphones', category: 'electronics', image: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/08/shopify-themes.jpg?auto=format&q=60&fit=max&w=930', link: 'https://www.shopify.com/'},
   ] 
 
   const { isWeb3Enabled } = props
 
   return (
     <ShopsContainer>
-        <div className="grid grid-cols-3 gap-4 content-evenly">
+        <StoresDiv className="grid grid-cols-3 gap-4 content-evenly">
             {shopifyStores.map((store) => {
                     return (
-                    <div className="m-5" style={{width: '250px'}}>
-                        <a href={store.link} rel="noopener noreferrer" target="_blank">
-                            <Card
-                                onClick={''}
-                            >
-                                <div>
-                                    {store.storeName}
-                                </div>
-                                <div>
-                                    {store.description}
-                                </div>
-                                <div>
-                                    <Illustration height={'180px'} logo={'token'} width={'100%'} />
-                                    {/* <img src={store.image}/> */}
-                                </div>
-                                <div style={{ display: 'flex', justifyContent: 'center'}}>
-                                    <a href={store.link} rel="noopener noreferrer" style={{ textDecoration: 'none' }} target="_blank" >
-                                        <Button
-                                            text='View Store'
-                                            customize={{
-                                                backgroundColor: '#9EC7EA',
-                                                onHover: 'darken',
-                                            }}
-                                            theme="custom"
-                                        />
-                                    </a>
-                                </div>
-                            </Card>
-                        </a>
-                    </div>
+                        <CardDiv className="m-5">
+                            <Link href={store.link} rel="noopener noreferrer" target="_blank">
+                                <Card
+                                    onClick={''}
+                                >
+                                    <StoreNameDiv>
+                                        {store.storeName}
+                                    </StoreNameDiv>
+                                    <StoreDescriptionDiv>
+                                        {store.description}
+                                    </StoreDescriptionDiv>
+                                    <ImageDiv>
+                                        <Image src={store.image}/>
+                                    </ImageDiv>
+                                    <ButtonDiv>
+                                        <Link href={store.link} rel="noopener noreferrer" target="_blank" >
+                                            <Button
+                                                text='View Store'
+                                                customize={{
+                                                    backgroundColor: '#9EC7EA',
+                                                    onHover: 'darken',
+                                                }}
+                                                theme="custom"
+                                            />
+                                        </Link>
+                                    </ButtonDiv>
+                                </Card>
+                            </Link>
+                        </CardDiv>
                     )
                 })
             }
-        </div>
+        </StoresDiv>
     </ShopsContainer>
   )
 }
--- components/Shops/ShopsStyles.js
+++ components/Shops/ShopsStyles.js
@@ -3,3 +3,39 @@ import styled from 'styled-components';
 export const ShopsContainer = styled.div`
 
 `
+
+export const StoresDiv = styled.div`
+
+`
+
+export const CardDiv = styled.div`
+    width: 250px;
+`
+
+export const StoreNameDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const StoreDescriptionDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const ImageDiv = styled.div`
+    margin: 5px;
+    margin-bottom: 15px;
+`
+
+export const Image = styled.img`
+
+`
+
+export const ButtonDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Link = styled.a`
+    text-decoration: none;
+`
